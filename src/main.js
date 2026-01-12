9677a7f04edf516f9012b17e6487658b29a3b9d2
zhaokangli <2480197118@qq.com>
2026-01-12 14:48:35 +0800

    主要导航栏菜单新建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import "./assets/main.css";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css';
 // if you just want to import css
 import 'element-plus/theme-chalk/dark/css-vars.css'
+// 3. 引入自定义暗黑样式（关键，覆盖默认变量）
+import './assets/dark-theme.css'
+import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
@@ -10,6 +12,9 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 console.log('当前环境',import.meta.env.MODE)
+
+// 关键：挂载前，强制给html根节点添加dark类名,开启暗黑模式
+document.documentElement.classList.add('dark')
 const app = createApp(App);
 
 app.use(createPinia());
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,21 +1,59 @@
 <template>
-    <div class="dark homeBox">
-        首页导航栏
-    </div>
+  <div class="homeBox">
+    <el-container>
+      <el-header style="padding: 0">
+        <el-menu
+          :default-active="activeIndex2"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+        >
+          <el-menu-item index="1">
+            <el-icon><Coin /></el-icon>
+            <span>首页</span>
+          </el-menu-item>
+          <el-sub-menu index="2">
+            <template #title>二级</template>
+            <el-menu-item index="2-1">item one</el-menu-item>
+            <el-menu-item index="2-2">item two</el-menu-item>
+          </el-sub-menu>
+          <el-menu-item index="3">
+            <el-icon><TrendCharts /></el-icon>
+            <span>k线展示图</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <el-icon><User /></el-icon>
+            <span>我的</span>
+          </el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>主体</el-main>
+    </el-container>
+  </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
+import { TrendCharts, Coin, User } from "@element-plus/icons-vue";
 
+const activeIndex2 = ref("1");
 
+const handleSelect = (key, keyPath) => {
+  console.log(key, keyPath);
+};
 
-
-
-onMounted(() => {
-
-})
+onMounted(() => {});
 </script>
 
 <style scoped lang="less">
-
+.el-menu-demo {
+  width: 100%;
+  padding-left: 100px;
+  height: 65px;
+}
+.homeBox {
+  .icon {
+    width: 15px;
+  }
+}
 </style>
